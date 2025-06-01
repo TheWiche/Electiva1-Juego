3e86e64eb5a5989eea9fa8ed6f574ff2bad5b9d1
Nolger Rodriguez <nolger@nolger.com>
2025-05-31 19:35:35 -0500

    Arreglo mayores

--- Assets/Scripts/SettingManager.cs
+++ Assets/Scripts/SettingManager.cs
@@ -2,22 +2,43 @@ using UnityEngine;
 
 public static class SettingsManager
 {
+    public const string RESOLUTION_INDEX_KEY = "resolutionIndex";
+    public const string FULLSCREEN_KEY = "fullscreen";
+    public const string VOLUME_KEY = "volume";
+
     public static void ApplySavedSettings()
     {
-        int resolutionIndex = PlayerPrefs.GetInt("resolutionIndex", 0);
-        bool fullscreen = PlayerPrefs.GetInt("fullscreen", 1) == 1;
-        float volume = PlayerPrefs.GetFloat("volume", 1f);
+        int resolutionIndex = PlayerPrefs.GetInt(RESOLUTION_INDEX_KEY, 0);
+        bool fullscreen = PlayerPrefs.GetInt(FULLSCREEN_KEY, 1) == 1;
+        float volume = PlayerPrefs.GetFloat(VOLUME_KEY, 1f);
 
-        Resolution[] resolutions = Screen.resolutions;
-        if (resolutionIndex < 0 || resolutionIndex >= resolutions.Length)
-            resolutionIndex = 0; // fallback
+        Resolution[] availableResolutions = Screen.resolutions;
 
-        Resolution res = resolutions[resolutionIndex];
-        
-        // Mejor control del modo pantalla
-        Screen.fullScreenMode = fullscreen ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
-        Screen.SetResolution(res.width, res.height, Screen.fullScreenMode);
-        
+        if (availableResolutions.Length == 0)
+        {
+            return;
+        }
+
+        if (resolutionIndex < 0 || resolutionIndex >= availableResolutions.Length)
+        {
+            resolutionIndex = 0;
+        }
+
+        Resolution targetResolution = availableResolutions[resolutionIndex];
+
+        FullScreenMode screenMode = fullscreen ? FullScreenMode.ExclusiveFullScreen : FullScreenMode.Windowed;
+        int preferredRefreshRate = 0;
+
+        Screen.SetResolution(targetResolution.width, targetResolution.height, screenMode, preferredRefreshRate);
+
         AudioListener.volume = volume;
+    }
+
+    public static void SaveSettings(int resolutionIndex, bool fullscreen, float volume)
+    {
+        PlayerPrefs.SetInt(RESOLUTION_INDEX_KEY, resolutionIndex);
+        PlayerPrefs.SetInt(FULLSCREEN_KEY, fullscreen ? 1 : 0);
+        PlayerPrefs.SetFloat(VOLUME_KEY, volume);
+        PlayerPrefs.Save();
     }
 }
